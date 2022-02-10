2819d9968d8cedea29fdeef3017df266aeac9a8e
mattwymant <70105697+mattwymant@users.noreply.github.com>
2022-02-10 21:32:39 +0000

    50% of Creating the Find Method Middle Layer lab sheet completed. No errors.

--- BookStore Testing/tstSupplier.cs
+++ BookStore Testing/tstSupplier.cs
@@ -17,6 +17,21 @@ namespace BookStore_Testing
         }
 
         [TestMethod]
+        public void SupplierNoOk()
+        {
+            //instance of the class
+            clsSupplier ASupplier = new clsSupplier();
+            //declare test data variable
+            Int32 TestData;
+            //create test data
+            TestData = 5;
+            //assign test data to property
+            ASupplier.SupplierNo = TestData;
+            //test to see if values match
+            Assert.AreEqual(TestData, ASupplier.SupplierNo);
+        }
+
+            [TestMethod]
         public void NameOk()
         {
             //instance of the class
@@ -106,7 +121,42 @@ namespace BookStore_Testing
             Assert.AreEqual(TestData, ASupplier.PhoneNumber);
         }
 
-        
+        [TestMethod]
+        public void FindMethodOk()
+        {
+            //instance of the class
+            clsSupplier ASupplier = new clsSupplier();
+            //boolean variable to for the validation results
+            Boolean Found = false;
+            //create test data
+            Int32 SupplierNo = 5;
+            //invoke the method
+            Found = ASupplier.Find(SupplierNo);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestSupplierNotFound()
+        {
+            //instance of the class
+            clsSupplier ASupplier = new clsSupplier();
+            //boolean variable to for the validation results
+            Boolean Found = false;
+            //boolean variable to record is data is OK
+            Boolean OK = true;
+            //create test data
+            Int32 SupplierNo = 5;
+            //invoke the method
+            Found = ASupplier.Find(SupplierNo);
+            //check the supplier no
+            if (ASupplier.SupplierNo != 5)
+            {
+                OK = false;
+            }
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
 
     }    
 }
--- BookStoreClasses/clsSupplier.cs
+++ BookStoreClasses/clsSupplier.cs
@@ -1,12 +1,43 @@
-﻿namespace BookStoreClasses
+﻿using System;
+
+namespace BookStoreClasses
 {
     public class clsSupplier
     {
+        //private data memeber for address no property
+        private Int32 mSupplierNo;
+        //public data memeber for address no property
+        public int SupplierNo
+        {
+            get
+            {
+                //this line of code send data out of the property
+                return mSupplierNo;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mSupplierNo = value;
+            }
+
+        }
+
         public string Name { get; set; }
         public double Cost { get; set; }
         public string Address { get; set; }
         public string Description { get; set; }
         public string PhoneNumber { get; set; }
         public string Postcode { get; set; }
+        
+
+        
+        //public find property
+        public bool Find(int SupplierNo)
+        {
+            //set the private data members to the test data value
+            mSupplierNo = 5;
+            //set to always return true
+            return true;
+        }
     }
 }
