efa158260e40b2057a4f81b9c8dedfe7222a9ab1
Matthew Wymant <P1621842X@my365.dmu.ac.uk>
2022-03-06 15:34:15 +0000

    06032022matt

--- BookStoreFrontOffice/ASupplier.aspx.cs
+++ BookStoreFrontOffice/ASupplier.aspx.cs
@@ -35,4 +35,38 @@ public partial class ASupplier : System.Web.UI.Page
     {
 
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the class
+        clsSupplier ASupplier = new clsSupplier();
+        //variable to store the primary key 
+        Int32 SupplierNo;
+        //variable to store the result of the find op
+        Boolean Found = false;
+        //Get the pk entered
+        SupplierNo = Convert.ToInt32(txtSupplierNo.Text);
+        //find the record
+        Found = ASupplier.Find(SupplierNo);
+        //if found
+        if (Found==true)
+        {
+            //display values of propeties in the form
+            txtName.Text = ASupplier.Name;
+            txtAddress.Text = ASupplier.Address;
+            txtPostcode.Text = ASupplier.Postcode;
+            txtCost.Text = ASupplier.Cost.ToString();
+            txtDescription.Text = ASupplier.Description;
+            txtPhoneNumber.Text = ASupplier.PhoneNumber;
+            
+        }
+        //if not found
+        else
+        {
+            //return error message
+            lblError.Text = "This Supplier Number does not exist! Please try again.";
+            lblError.Visible = true;
+            lblError.ForeColor = System.Drawing.Color.Red;
+        }
+    }
 }
