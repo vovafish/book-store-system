515fcc6915da4a096bd19f14d6b6405a972f4d15
p2598200 <p2598200@GWHXX683WIN005W>
2022-03-10 14:14:46 +0000

    Staff Schedule

--- BookStoreFrontOffice/StaffManagement/StaffTimeTable.aspx.cs
+++ BookStoreFrontOffice/StaffManagement/StaffTimeTable.aspx.cs
@@ -11,4 +11,9 @@ public partial class StaffManagement_StaffTimeTable : System.Web.UI.Page
     {
 
     }
+
+    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
 }
