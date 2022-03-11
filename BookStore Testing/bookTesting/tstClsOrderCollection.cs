d2b5e285d9ba4e9ea7e0182c4968a34e377b73e5
P2598678 <P2598678@KLIXX104WIN019W>
2022-03-11 20:34:58 +0000

    starting adding tst collections

---
+++ BookStore Testing/bookTesting/tstClsOrderCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookStoreClasses;
+
+
+
+namespace BookStoreClasses
+{
+    [TestClass]
+    class clsOrderCollection
+    {
+        public int Order { get; private set; }
+
+        public void InstanceOK()
+        {
+            //creates instance of class 
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //test to see if its there 
+            Assert.IsNotNull(AllOrders);
+        }
+
+        [TestMethod]
+        public void OrderPropertyOK()
+        {
+            clsOrderCollection AllOrders = new clsOrderCollection();
+
+            Int32 SomeOrder = 1;
+
+            AllOrders.Order = SomeOrder;
+
+            Assert.AreEqual(AllOrders.Order, SomeOrder);
+        }
+
+        public void AllOrdersOK()
+        {
+
+        }
+       
+    }
+}
