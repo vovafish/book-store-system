c4914adde47a39967c11dbd996f8de0259f70956
Matthew Wymant <P1621842X@my365.dmu.ac.uk>
2022-03-17 18:48:10 +0000

    Week 23 completed

--- BookStoreClasses/clsSupplierCollection.cs
+++ BookStoreClasses/clsSupplierCollection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace BookStoreClasses
 {
@@ -6,6 +7,41 @@ namespace BookStoreClasses
     {
         //private data memeber for the list
         List<clsSupplier> mSupplierList = new List<clsSupplier>();
+
+        //contructor for class
+        public clsSupplierCollection()
+        {
+            
+            //var for index
+            Int32 Index = 0;
+            //var to store the count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblSupplier_SelectAll");
+            //get the count of the records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                clsSupplier ASupplier = new clsSupplier();
+                //set it's properties
+                ASupplier.SupplierNo =  Convert.ToInt32(DB.DataTable.Rows[Index]["SupplierNo"]);
+                ASupplier.Name =        Convert.ToString(DB.DataTable.Rows[Index]["Name"]);
+                ASupplier.Address =     Convert.ToString(DB.DataTable.Rows[Index]["Address"]);
+                ASupplier.Postcode =    Convert.ToString(DB.DataTable.Rows[Index]["Postcode"]);
+                ASupplier.PhoneNumber = Convert.ToString(DB.DataTable.Rows[Index]["PhoneNumber"]);
+                ASupplier.Description = Convert.ToString(DB.DataTable.Rows[Index]["Description"]);
+                ASupplier.Cost =        Convert.ToInt32(DB.DataTable.Rows[Index]["Cost"]);
+                //Add item to test list
+                mSupplierList.Add(ASupplier);
+                //index to next record
+                Index++;
+            }
+            
+            
+        }
 
         //public property for supplier list
         public List<clsSupplier> SupplierList
---
+++ BookStoreFrontOffice/book-templates/SupplierDefault.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SupplierDefault : System.Web.UI.Page
+{
+    //this function handles the load event 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if this is the first visit
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplaySuppliers();
+        }
+
+    }
+
+    
+    void DisplaySuppliers()
+    {
+        //create an instance of the county collection 
+        BookStoreClasses.clsSupplierCollection Suppliers = new BookStoreClasses.clsSupplierCollection();
+        //set the data source to the list of suppliers in the collection
+        LstSupplier.DataSource = Suppliers.SupplierList;
+        //set the name of the pk 
+        LstSupplier.DataValueField = "SupplierNo";
+        //set the data field to display
+        LstSupplier.DataTextField = "Name";
+        //bind the data to the list 
+        LstSupplier.DataBind();
+    }
+
+    protected void LstSupplier_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void DdlSupplier_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TxtSearchSupplier_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSearch_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSelectSupplier_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnHome_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnBooks_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSupplier_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnUsers_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnOrders_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSignUp_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnStaff_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnAddSupplierPage_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnEditSupplierPage_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnDeleteSupplierPage_Click(object sender, EventArgs e)
+    {
+
+    }
+}
