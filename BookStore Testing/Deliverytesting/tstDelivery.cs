bb9a6c8796035d80e34bd4b1a8c20c2ecbf32ac0
katBravo <p2407495@my365.dm.ac.uk>
2022-08-08 17:27:26 +0100

    added testing for delivery class

---
+++ BookStore Testing/Deliverytesting/tstDelivery.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BookStore_Testing.Deliverytesting
+{
+    [TestClass]
+    public class tstDelivery
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+           
+                //create an instance of the class we want to create
+                clsDelivery ADelivery = new clsDelivery();
+                //test to see that it exists
+                Assert.IsNotNull(ADelivery);
+            
+        }
+    }
+}
